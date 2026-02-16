7c1e348c14981225f440723802aabe56a0d53c46
sredhu37 <sunny.redhu@trivago.com>
2026-02-16 17:19:10 +0100

    test change

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-// Test the CI cache again
+// Test the CI cache
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
